dbded4eb033dd5c2aaf9ee27eac4dd69b29f8598
Jhonatan <jhonatantiradotiradodeep@gmail.com>
2019-12-05 18:51:27 -0500

    Push notifications

---
+++ lib/screen/messages/messaging_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:inkapharma/widget/messaging_widget.dart';
+
+class MessagingPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: MessagingWidget(),
+  );
+}
