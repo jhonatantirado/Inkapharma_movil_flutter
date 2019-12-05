dfb93f889dbd2c77c494e7cfdc779cbe063d5d16
Jhonatan <jhonatantiradotiradodeep@gmail.com>
2019-12-05 13:57:45 -0500

    Remove not used code

--- lib/infraestructure/CourseRepository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:inkapharma/model/course.dart';
-
-abstract class CourseRepository {
-  Future<int> insert(Course course);
-  Future<int> update(Course course);
-  Future<int> delete(Course course);
-  Future<List<Course>> getList();
-}
